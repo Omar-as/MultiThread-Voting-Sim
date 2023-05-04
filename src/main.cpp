75bee8ce824556c6726eae616c410533c7954de9
Ahmad-Jarir <ahmadjareer@gmail.com>
2023-05-04 16:40:06 +0300

    added -c flag

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,23 @@
 int main(int argc, char **argv) { 
     argparse::ArgumentParser program("voting simulator");
     program
-        .add_argument("-t", "--time")
+        .add_argument("-t")
         .required()
-        .help("specify the time")
+        .help("specify the time for each voter to arrive")
         .scan<'i', int>();
         
     program
-        .add_argument("-p", "--probability")
+        .add_argument("-p")
         .required()
-        .help("specify the probability")
+        .help("specify the probability of a voter arriving")
         .scan<'f', float>();
+
+    program
+        .add_argument("-c")
+        .help("specify the number of polling stations")
+        .default_value(1)
+        .scan<'i', int>();
+
 
     try {
       program.parse_args(argc, argv);
@@ -23,12 +30,16 @@ int main(int argc, char **argv) {
       std::cerr << program;
       return 1;
     }
-    auto time        = program.get<int>("-t");
-    auto probability = program.get<float>("-p");
+
+    auto time               = program.get<int>("-t");
+    auto probability        = program.get<float>("-p");
+    auto number_of_stations = program.get<int>("-c");
 
     std::cout << "time: " << time << "\n";
 
     std::cout << "prob: " << probability << "\n";
+
+    std::cout << "stations: " << number_of_stations << "\n";
 
 
     return 0;
