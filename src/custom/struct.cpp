1a6105d375c6e69faabd84a5735cefa0aa3bb0dc
omar-as <omarelassaad93@gmail.com>
2023-05-05 15:22:05 +0300

    Split files

---
+++ src/custom/struct.cpp
@@ -0,0 +1,14 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+namespace custom {
+
+   // Voter creation arguments struct
+   struct args_struct {
+       float probability;
+       int sim_time;
+       /* int station_number; */
+   };
+
+}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,12 @@ using namespace std;
 /* Custom Imports */
 
 #include "custom/sleep.cpp"
+#include "custom/struct.cpp"
+#include "custom/voter.cpp"
 
 /* Other Imports */
 
 #include <argparse/argparse.hpp>
-#include <vector>
 #include <random>
 #include <chrono>
 
@@ -26,20 +27,11 @@ using namespace std;
 /* Global State */
 /****************/
 
-// Voter creation arguments struct
-struct args_struct {
-    float probability;
-    int sim_time;
-    /* int station_number; */
-};
-
 /******************************************************************************/
 
 /*************************/
 /* Function Declarations */
 /*************************/
-
-void* create_voters( void* args_ptr );
 
 /******************************************************************************/
 
@@ -111,10 +103,10 @@ int main(int argc, char **argv) {
     pthread_t creation_thread;
 
     // Set values of the struct
-    args_struct args = { probability, time };
+    custom::args_struct args = { probability, time };
 
     // Send values to create voters
-    pthread_create( &creation_thread, NULL, create_voters, (void*) &args );
+    pthread_create( &creation_thread, NULL, custom::create_voters, (void*) &args );
 
     // Join Threads
     pthread_join(creation_thread, NULL);
@@ -127,65 +119,5 @@ int main(int argc, char **argv) {
 /********************/
 /* Helper Functions */
 /********************/
-
-void* create_voters( void* args_ptr )
-{
-    // variables from struct
-    float probability;
-    int sim_time;
-    int station_number;
-
-    auto args = *(args_struct *) args_ptr;
-    probability = args.probability;
-    sim_time = args.sim_time;
-    /* station_number = args.station_number; */
-
-    // Time per voter 
-    const auto t = 1;
-
-    // Ticket Counter
-    auto next_ticket = 1;
-
-    // Voters "Queues"
-    vector<int> ordinary_voters = {};
-    vector<int> special_voters = {};
-
-    // Simulation timer
-    auto current_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
-    auto end_sim_time =  static_cast<int>(current_time) + sim_time;
-
-    while(current_time < end_sim_time){
-
-        // Random value gen for probability
-        random_device rd;
-        mt19937 gen(rd());
-        uniform_real_distribution<> dis(0, 1);
-
-        double random_number = dis(gen);
-
-        if (random_number <= probability) { ordinary_voters.push_back(next_ticket); }
-        else { special_voters.push_back(next_ticket); }
-
-        next_ticket++;
-
-        // Thread sleeps for t secs
-        custom::pthread_sleep(t);
-        current_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
-    }
-
-    /* cout << "station [" << station_number << "] finished creating voters" << endl; */
-
-    cout << "Ordinary Voters:" << endl;
-    for(int i : ordinary_voters){
-        cout << " " << i;
-    }
-    cout << endl << "Special Voters:" << endl;
-    for(int i : special_voters){
-        cout << " " << i;
-    }
-    cout << endl;
-
-    return EXIT_SUCCESS;
-}
 
 /******************************************************************************/
